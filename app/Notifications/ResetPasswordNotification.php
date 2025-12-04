5ebb51bc0b2e7cfd159b4837cb5c6ef13158958c
Alfredo Palacios <joseaph.1998@gmail.com>
2025-12-04 13:51:35 -0600

    feat: custom notification implemtented

---
+++ app/Notifications/ResetPasswordNotification.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ResetPasswordNotification extends Notification
+{
+    use Queueable;
+
+    private string $token;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(string $token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        $url = config('app.frontend_url') . "/reset-password?token=$this->token&email=$notifiable->email";
+
+        return (new MailMessage)
+            ->subject('Restablecer contraseña')
+            ->line('Estás recibiendo este correo porque solicitaste un restablecimiento de contraseña.')
+            ->action('Restablecer contraseña', $url)
+            ->line('Si no solicitaste este cambio, ignora este correo.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
