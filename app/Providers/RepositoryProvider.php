4081bc1df473c1ce760f0bac349a2f916ce79a99
Alfredo Palacios <joseaph.1998@gmail.com>
2025-12-04 12:42:23 -0600

    feat: provider for repositories

---
+++ app/Providers/RepositoryProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repositories\Contract\UserRepositoryInterface;
+use App\Repositories\Eloquent\UserRepository;
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
