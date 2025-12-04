1f01ae4b268af19f437c95decaa68d3df5088291
Alfredo Palacios <joseaph.1998@gmail.com>
2025-12-04 13:49:52 -0600

    feat: controller created to handle reset passwords

---
+++ app/Http/Controllers/ForgetPasswordController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ForgetPasswordRequest;
+use App\Http\Resources\EmailResource;
+use App\Http\Resources\ErrorResource;
+use App\Repositories\Contract\UserRepositoryInterface;
+use Illuminate\Support\Facades\Password;
+
+class ForgetPasswordController extends Controller
+{
+    public function __construct(private readonly UserRepositoryInterface $userRepository)
+    {
+    }
+
+    public function sendResetLink(ForgetPasswordRequest $request): EmailResource|ErrorResource
+    {
+        $user = $this->userRepository->getByField('email', $request->validated('email'));
+
+        if (!$user) {
+            return new ErrorResource('Este correo no se encuentra registrado');
+        }
+
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        if ($status !== Password::RESET_LINK_SENT) {
+            return new ErrorResource("No se pudo enviar el correo por $status");
+        }
+
+        return new EmailResource($status);
+    }
+}
