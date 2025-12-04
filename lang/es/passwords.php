f157c4ca1656f2b86b58c18b046f2a1efe40030c
Alfredo Palacios <joseaph.1998@gmail.com>
2025-12-04 16:24:28 -0600

    feat: text added

--- lang/es/passwords.php
+++ lang/es/passwords.php
@@ -4,6 +4,7 @@ return [
     'reset' => 'Tu contraseña ha sido restablecida.',
     'sent' => 'Te hemos enviado el enlace para restablecer tu contraseña.',
     'throttled' => 'Por favor espera antes de volver a intentarlo.',
+    'trouble_clicking' => 'Si tienes problemas para hacer clic en el botón ":actionText", copia y pega la siguiente URL en tu navegador:',
     'token' => 'Este token para restablecer la contraseña no es válido.',
     'user' => 'No pudimos encontrar un usuario con ese correo electrónico.',
 ];
