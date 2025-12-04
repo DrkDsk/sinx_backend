c6ea269127ecb82ea1dbcc8661aa3d2e7e93ef66
Alfredo Palacios <joseaph.1998@gmail.com>
2025-12-04 13:50:45 -0600

    feat: route to sendEmail created

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ForgetPasswordController;
 use Illuminate\Support\Facades\Route;
+
+Route::prefix('password')->group(function () {
+    Route::post('forgot-password', [ForgetPasswordController::class, 'sendResetLink']);
+});
 
 Route::prefix('auth')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
